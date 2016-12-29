ddaba37a10b150cd2f58f4c3f6c460efdd653b02
DanielNegrea <daniel.negrea85@gmail.com>
2016-12-29 15:56:47 +0200

    Varianta 2

--- Laborator_10.java
+++ Laborator_10.java
@@ -2,7 +2,7 @@ public class Laborator_10
 {
     private Circle cercGalben;
     private Triangle triunghiAlbastru;
-
+    private Square patratRosu;
     /**
      * Constructor for objects of class Laborator_10
      */
@@ -11,15 +11,27 @@ public class Laborator_10
         // initialise instance variables
         cercGalben = new Circle();
         triunghiAlbastru = new Triangle();
-        cercGalben.changeColor("yellow");
-        cercGalben.changeSize(100);
-        cercGalben.moveHorizontal(90);
-        cercGalben.moveLeft();
-        cercGalben.makeVisible();
+        patratRosu = new Square();
+        patratRosu.changeColor("red");
+        patratRosu.changeSize(100);        
+        patratRosu.moveDown();
+        patratRosu.moveDown();
+        patratRosu.moveDown();
+        patratRosu.moveRight();
+        patratRosu.makeVisible();
         triunghiAlbastru.changeColor("blue");
         triunghiAlbastru.changeSize(85, 130);
-        triunghiAlbastru.moveHorizontal(90);
-        triunghiAlbastru.makeVisible();
+        triunghiAlbastru.moveHorizontal(80);
+        triunghiAlbastru.moveDown();
+        triunghiAlbastru.makeVisible();        
+        cercGalben.changeColor("yellow");
+        cercGalben.changeSize(70);
+        cercGalben.moveHorizontal(160);
+        cercGalben.moveUp();
+        cercGalben.moveUp();
+        cercGalben.moveUp();
+        cercGalben.moveRight();
+        cercGalben.makeVisible();
     }
 
    
